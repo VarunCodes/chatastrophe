328e27eccbb874d062081d8609dbb5133d0ee04b
Varun Madhukara <varun@Varuns-MacBook-Pro.local>
2018-12-01 03:13:06 +0000

    webpack dev server

--- webpack.config.js
+++ webpack.config.js
@@ -4,5 +4,10 @@ module.exports = {
    path: __dirname + "/public",
    filename: "bundle.js",
    publicPath: "/",
-  }
+ },
+  devServer: {
+  contentBase: "./public", //sets /public as the folder to serve
+  historyApiFallback: true, //lets our single page app seem like a multi page app
+  inline: true, //refreshes page on file change
+}
 };
