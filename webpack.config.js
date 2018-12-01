45660764dd906d2aa96e2e62fb43a5488bee86f5
Varun Madhukara <varun@Varuns-MacBook-Pro.local>
2018-12-01 05:41:28 +0000

    babel, webpack update, ES6

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,26 @@
 module.exports = {
-  entry:  __dirname + "/src/index.js",
+  entry: __dirname + "/src/index.js",
   output: {
    path: __dirname + "/public",
    filename: "bundle.js",
-   publicPath: "/",
- },
+   publicPath: "/"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/, //babel transpiles all files ending in .js
+        exclude: /node_modules/, //we are excluding node_modules because it is already in ES5
+        loader: 'babel-loader', //name of our loader
+        query: {
+         presets: ['es2015','react'],
+         plugins: ['transform-class-properties'] //query to define our presets (which Babel uses to transpile)
+        }
+      },
+    ]
+  },
   devServer: {
   contentBase: "./public", //sets /public as the folder to serve
   historyApiFallback: true, //lets our single page app seem like a multi page app
-  inline: true, //refreshes page on file change
-}
+  inline: false, //refreshes page on file change
+  }
 };
